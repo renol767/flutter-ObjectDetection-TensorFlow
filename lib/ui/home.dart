181fce7c78cd3af0fd6c8fd287096613b638db95
renol767 <72129061+renol767@users.noreply.github.com>
2021-05-20 06:40:16 +0700

    2x back button exit from application

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tensorflow_objectdetection/image_classification.dart/homepage.dart';
 import 'package:tensorflow_objectdetection/ui/home_view.dart';
 import 'package:tensorflow_objectdetection/ui/how_to_use.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -15,104 +17,122 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(16.0),
-          margin: EdgeInsets.all(8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 80,
-              ),
-              Image.asset("assets/menu.png"),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Menu",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              FlatButton(
-                  color: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  height: 50,
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HomeView()));
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.timer,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        " Real Time Detection",
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              FlatButton(
-                  color: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  height: 50,
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MyHomePage()));
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.photo,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        "  Image Classification",
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              FlatButton(
-                  color: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  height: 50,
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HowToUse()));
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.info,
-                        color: Colors.white,
-                      ),
-                      Text("  How To Use",
-                          style: TextStyle(color: Colors.white))
-                    ],
-                  ))
-            ],
+      body: WillPopScope(
+        onWillPop: onWillPop,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Image.asset("assets/menu.png"),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Menu",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                FlatButton(
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    height: 50,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => HomeView()));
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.timer,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          " Real Time Detection",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                FlatButton(
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    height: 50,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyHomePage()));
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.photo,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "  Image Classification",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                FlatButton(
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    height: 50,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => HowToUse()));
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.info,
+                          color: Colors.white,
+                        ),
+                        Text("  How To Use",
+                            style: TextStyle(color: Colors.white))
+                      ],
+                    ))
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  DateTime currentBackPressTime;
+  Future<bool> onWillPop() {
+    DateTime now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime) > Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      Fluttertoast.showToast(msg: "Tekan Lagi Untuk Keluar");
+      return Future.value(false);
+    }
+    SystemNavigator.pop();
+    return Future.value(true);
   }
 }
